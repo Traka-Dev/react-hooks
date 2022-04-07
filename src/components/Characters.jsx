e8f01145b5ee9c95e48482d9547e15c7d44dd872
TrakaDev <trakadev@gamil.com>
2022-04-07 18:09:27 -0500

    [FIX] Characters Img Alt

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,50 +1,50 @@
-import React, {useState, useEffect, useReducer} from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import '../styles/Characters.css'
 
 export const Characters = () => {
 
     const initialState = {
-        favorites:[]
+        favorites: []
     }
 
     const favReducer = (state, action) => {
-        switch(action.type){
+        switch (action.type) {
             case 'ADD_TO_FAV':
                 return {
-                    ...state, 
-                    favorites:[
+                    ...state,
+                    favorites: [
                         ...state.favorites, action.payload
                     ]
                 }
             default:
-                return state    
+                return state
         }
     }
     const [favorites, dispatch] = useReducer(favReducer, initialState)
     const [characters, setCharacters] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character/')
-        .then(response => response.json())
-        .then(data => setCharacters(data.results))
-    },[])
+            .then(response => response.json())
+            .then(data => setCharacters(data.results))
+    }, [])
 
     const handleFavClick = fav => {
-        dispatch(favorites, {type:'ADD_TO_FAV', payload:fav})
+        dispatch(favorites, { type: 'ADD_TO_FAV', payload: fav })
     }
 
     return (
         <div className="characters">
             {favorites.favorites.map(fav => (
-               <li key={fav.id}>{fav.name} test</li>
+                <li key={fav.id}>{fav.name} test</li>
             ))}
             {characters.map(character => (
                 <div className="charCard" key={`char-${character.id}`}>
-                    <img className="charImg" src={character.image}></img>
+                    <img className="charImg" src={character.image} alt={character.name} />
                     <h2 className="charName">{character.name}</h2>
                     <p>status: {character.status}</p>
                     <p>origin: {character.origin.name}</p>
-                    <button type='button' onClick={() => handleFavClick(character) }>Agregar a Favoritos</button>
+                    <button type='button' onClick={() => handleFavClick(character)}>Agregar a Favoritos</button>
                 </div>
             ))}
         </div>
