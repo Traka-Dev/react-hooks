85048fc3b2a59179b133633833aef4b6ea6abd80
TrakaDev <trakadev@gamil.com>
2022-04-07 18:49:26 -0500

    [ADD] Filter Characters

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect, useReducer, useMemo } from 'react'
 import '../styles/Characters.css'
 
 export const Characters = () => {
@@ -22,6 +22,7 @@ export const Characters = () => {
     }
     const [favorites, dispatch] = useReducer(favReducer, initialState)
     const [characters, setCharacters] = useState([])
+    const [search, setSearch] = useState('')
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character/')
@@ -38,20 +39,37 @@ export const Characters = () => {
         }
     }
 
+    const handleSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
+    // const filteredUsers = characters.filter((user) => user.name.toLowerCase().includes(search.toLowerCase()))
+
+    const filteredUsers = useMemo(() => (
+        characters.filter((user) => user.name.toLowerCase().includes(search.toLowerCase()),
+            [characters, search]
+        )
+    ))
+
     return (
-        <div className="characters">
+        <>
             {favorites.favorites.map(fav => {
                 return (<li key={fav.id}>{fav.name}</li>)
             })}
-            {characters.map(character => (
-                <div className="charCard" key={`char-${character.id}`}>
-                    <img className="charImg" src={character.image} alt={character.name} />
-                    <h2 className="charName">{character.name}</h2>
-                    <p>status: {character.status}</p>
-                    <p>origin: {character.origin.name}</p>
-                    <button type='button' onClick={() => handleFavClick(character)}>Agregar a Favoritos</button>
-                </div>
-            ))}
-        </div>
+            <div className='Search'>
+                <input type='text' value={search} onChange={(e) => handleSearch(e)} />
+            </div>
+            <div className="characters">
+                {filteredUsers.map(character => (
+                    <div className="charCard" key={`char-${character.id}`}>
+                        <img className="charImg" src={character.image} alt={character.name} />
+                        <h2 className="charName">{character.name}</h2>
+                        <p>status: {character.status}</p>
+                        <p>origin: {character.origin.name}</p>
+                        <button type='button' onClick={() => handleFavClick(character)}>Agregar a Favoritos</button>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
